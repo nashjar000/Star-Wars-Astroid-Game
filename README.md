f36545746e648ac49191ac2783fc054a859b0e32
Jared Nash <104384562+nashjar000@users.noreply.github.com>
2024-06-08 09:49:39 -0600

    First commit

---
+++ README.md
@@ -0,0 +1,35 @@
+# Overview
+
+For this project, I wanted to improve my programming skills by writing a simple game using a Python game framework. My goal was to get the game to display graphics, take user input from the keyboard or mouse, and have moveable objects. 
+
+For the game, I made an Asteroids Clone, where players pilot a spaceship to destroy asteroids. To change it up a bit, I made it into a Star Wars themed game where a user is the Millennium Falcon and can fly around and destroy the asteroids. To play the game, users first need to run the program. While the program is running, users use the arrow keys to move the Millennium Falcon up and down, or left and right. To shoot lasers, the player needs to hit the space bar to fire at an astroid. 
+
+The purpose in writing this software was so that I could gain experience in writing code for games, improve my skills in the python language, and deepen my understanding of learning about a new type of programming.  
+
+{Provide a link to your YouTube demonstration.  It should be a 4-5 minute demo of the game being played and a walkthrough of the code.}
+
+[Software Demo Video](https://youtu.be/s6bwpjEvAaI)
+
+# Development Environment
+
+The tools I used for this project was VS Code, which is a free app to write code on a local computer. The programming language I used for this project was Python, using the Pygame extension and libraries. 
+
+{Describe the programming language that you used and any libraries.}
+The language I used was Python. The pygmy extension allowed me to set up a project to run a game on. The libraries that I used from pygmy were the pygmy.mixer, which lets me add sound and music to the game. I also used the random library to let me generate random asteroids in the game. The library for sys is for the game counsel window itself for users to open and close the game. 
+
+# Useful Websites
+
+{Make a list of websites that you found helpful in this project}
+* [RealPython](https://realpython.com/arcade-python-game-framework/)
+* [Pygame](https://www.pygame.org/news)
+* [Python_Arcade] (https://api.arcade.academy/en/latest/examples/index.html#sound)
+* [PygameColorObject] (https://www.tutorialspoint.com/pygame/pygame_color_object.htm)
+* [StarWarsSoundEffects] (https://www.soundboard.com/sb/starwarsfx)
+* [AsteroidField8BitVersion] (https://youtu.be/zEy8EQ6y5TM)
+
+# Future Work
+
+{Make a list of things that you need to fix, improve, and add in the future.}
+* Make a Startup Menu
+* Make levels 
+* Point system for each astroid that's been destroyed 
---
+++ starWars.py
@@ -0,0 +1,154 @@
+import pygame
+import sys
+import random
+import pygame.mixer
+
+# Initialize Pygame
+pygame.init()
+
+# Set the screen dimensions
+screen_width = 640 
+screen_height = 480
+screen = pygame.display.set_mode((screen_width, screen_height))
+
+# Set the title of the window
+pygame.display.set_caption('Star Wars Astroid')
+
+# Load the spaceship image
+spaceship_image = pygame.image.load('millennium_falcon.png')
+spaceship_image = pygame.transform.scale(spaceship_image, (30, 30))
+
+# Load the asteroid image
+asteroid_image = pygame.image.load('astroid.png')
+asteroid_image = pygame.transform.scale(asteroid_image, (20, 20))
+
+# Define some colors
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+
+# Define the spaceship speed
+spaceship_speed = 5
+
+# Define the asteroid speed
+asteroid_speed = 2
+
+# Define the initial spaceship position
+spaceship_x = screen_width / 2
+spaceship_y = screen_height / 2
+
+# Lasers:
+class Laser:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.width = 10
+        self.height = 5
+        self.color = (255, 0, 0, 255) # red
+        self.speed = -10
+
+        # Load and play the laser sound effect
+        pygame.mixer.init()
+        laser_sound = pygame.mixer.Sound('x-wing_fire.mp3')
+        laser_sound.set_volume(0.5)
+        laser_sound.play()
+
+    def update(self):
+        self.x += self.speed
+
+    def draw(self, screen):
+        pygame.draw.rect(screen, self.color, (self.x, self.y, self.width, self.height))
+
+lasers = []
+
+# Generate asteroids 
+def generate_asteroids():
+    asteroids = []
+    for i in range(5):
+        asteroid_x = random.randint(0, screen_width // 2)
+        asteroid_y = random.randint(0, screen_height - 20)
+        asteroids.append((asteroid_x, asteroid_y))
+    return asteroids
+
+# Music:
+pygame.mixer.init()
+pygame.mixer.music.set_volume(1)  
+sound = pygame.mixer.Sound('StarWarsAstroidField.mp3')
+pygame.mixer.music.load('StarWarsAstroidField.mp3')
+pygame.mixer.music.play(-1, 1)  # -1 means play indefinitely, 1 means loop
+
+asteroids = generate_asteroids()
+explosion_sound = pygame.mixer.Sound('TIE_fighter_explode.mp3')
+explosion_sound.set_volume(0.5)  # Adjust the volume as needed
+
+while True:
+    # Handle events
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                lasers.append(Laser(spaceship_x + 20, spaceship_y + 15))
+
+    # Get the current key presses
+    keys = pygame.key.get_pressed()
+
+    # Move the spaceship
+    if keys[pygame.K_UP]:
+        spaceship_y -= spaceship_speed
+    if keys[pygame.K_DOWN]:
+        spaceship_y += spaceship_speed
+    if keys[pygame.K_LEFT]:
+        spaceship_x -= spaceship_speed
+    if keys[pygame.K_RIGHT]:
+        spaceship_x += spaceship_speed
+
+    # Move and draw the lasers
+    for laser in lasers:
+        laser.update()
+        laser.draw(screen)
+
+        # Remove lasers that are off the screen
+        if laser.x < 0:
+            lasers.remove(laser)
+
+    # Move the asteroids
+    for i, (asteroid_x, asteroid_y) in enumerate(asteroids):
+        asteroid_x += asteroid_speed
+        if asteroid_x > screen_width:
+            asteroid_x = 0
+        asteroids[i] = (asteroid_x, asteroid_y)
+
+    # Check for collisions
+    if not asteroids:
+        asteroids = generate_asteroids()
+
+    for asteroid_x, asteroid_y in asteroids:
+        if (spaceship_x - 15 < asteroid_x < spaceship_x + 15 and
+            spaceship_y - 15 < asteroid_y < spaceship_y + 15):
+            print("Game Over!")
+            pygame.quit()
+            sys.exit()
+
+        for laser in lasers:
+            if (laser.x + laser.width > asteroid_x and
+                laser.x < asteroid_x + 20 and
+                laser.y + laser.height > asteroid_y and
+                laser.y < asteroid_y + 20):
+                asteroids.remove((asteroid_x, asteroid_y))
+                lasers.remove(laser)
+                explosion_sound.play()
+                break
+
+    # Draw everything
+    screen.fill(BLACK)
+    screen.blit(spaceship_image, (spaceship_x, spaceship_y))
+    for asteroid_x, asteroid_y in asteroids:
+        screen.blit(asteroid_image, (asteroid_x, asteroid_y))
+    for laser in lasers:
+        laser.draw(screen)
+
+    # Update the screen
+    pygame.display.flip()
+    pygame.time.Clock().tick(60) 
+
